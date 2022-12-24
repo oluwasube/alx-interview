ecd4d2dbf151744b67d7cf1b94062bd8ab13ca94
Oluwasube promise <enibeke20@gmail.com>
2022-12-24 14:26:44 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """Pascal Triangle Interview Challenge"""
 def pascal_triangle(n):
+    # Return an empty list if n is less than or equal to 0
     if n <= 0:
         return []
     
-    triangle = [[1]]
-    
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
+    triangle = []
+    for i in range(n):
+        # The first and last elements in each row are always 1
+        row = [1] * (i + 1)
+        if i > 1:
+            # Generate the values for the rest of the elements in the row
+            for j in range(1, i):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
-    
     return triangle
-
