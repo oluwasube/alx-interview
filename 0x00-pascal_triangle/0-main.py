69082f9c2bcf3355eed7b1de12930e1bfda5001b
oluwasube <enibeke20@gmail.com>
2022-12-24 14:20:04 +0100

    pascal triangle

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -14,3 +14,5 @@ def print_triangle(triangle):
 
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
+    
+    
--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """Pascal Triangle Interview Challenge"""
-
 def pascal_triangle(n):
-    triangle = []
     if n <= 0:
-        return triangle
-
-    for i in range(n):
-        if i == 0:
-            triangle.append([1])
-        else:
-            row = []
-            for j in range(i+1):
-                if j == 0 or j == i:
-                    row.append(1)
-                else:
-                    row.append(triangle[i-1][j-1] + triangle[i-1][j])
-            triangle.append(row)
-
+        return []
+    
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    
     return triangle
+
