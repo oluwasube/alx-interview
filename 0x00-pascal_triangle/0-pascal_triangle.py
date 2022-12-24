2029531f8fa626d692d58d04f19f0b697b1c7109
oluwasube <enibeke20@gmail.com>
2022-12-24 14:36:52 +0100

    updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,13 +5,23 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     
-    triangle = []
+    triangle = [0] * n
+
     for i in range(n):
-        # The first and last elements in each row are always 1
-        row = [1] * (i + 1)
-        if i > 1:
-            # Generate the values for the rest of the elements in the row
-            for j in range(1, i):
+          # The first and last elements in each row are always 1
+        row = [0] * (i+1)
+        row[0] = 1
+        row[len(row) - 1] = 1
+        
+         # Generate the values for the rest of the elements in the row
+        for j in range(1, i):
+            if j > 0 and j < len(row):
                 row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
     return triangle
+
+
+    
+    
+           
+           
