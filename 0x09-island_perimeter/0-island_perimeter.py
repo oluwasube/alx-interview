9deb1ef69af74d05aff11546064ea3a5a690cf6e
oluwasube <enibeke20@gmail.com>
2023-02-28 15:50:14 +0100

    task done

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,23 +1,20 @@
-#!/usr/bin/python3
-
 def island_perimeter(grid):
-    """
-    Returns the perimeter of the island described in grid.
-    """
+    if not grid:
+        return 0
+
     perimeter = 0
     for i in range(len(grid)):
-        for j in range(len(grid[0])):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                # check left
-                if j == 0 or grid[i][j-1] == 0:
-                    perimeter += 1
-                # check right
-                if j == len(grid[0])-1 or grid[i][j+1] == 0:
-                    perimeter += 1
-                # check top
-                if i == 0 or grid[i-1][j] == 0:
-                    perimeter += 1
-                # check bottom
-                if i == len(grid)-1 or grid[i+1][j] == 0:
-                    perimeter += 1
+                boundaries = 4
+                if i > 0 and grid[i-1][j] == 1:
+                    boundaries -= 1
+                if i < len(grid)-1 and grid[i+1][j] == 1:
+                    boundaries -= 1
+                if j > 0 and grid[i][j-1] == 1:
+                    boundaries -= 1
+                if j < len(grid[i])-1 and grid[i][j+1] == 1:
+                    boundaries -= 1
+                perimeter += boundaries
+
     return perimeter
