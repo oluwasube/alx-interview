22510dbbae28b81bc6bef41752ca675b46026f27
oluwasube <enibeke20@gmail.com>
2023-02-28 16:01:19 +0100

    fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,33 +1,12 @@
 def island_perimeter(grid):
-    """
-    Calculates the perimeter of an island
-    """
+    """Calculate and return the perimeter of the island in the grid."""
     perimeter = 0
-    
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 1:
-                perimeter += check_adjacent_squares(grid, row, col)
-                
-    return perimeter
-
-
-def check_adjacent_squares(grid, row, col):
-    """
-    Checks the adjacent squares to the current square
-    """
-    perimeter = 0
-    
-    if col == 0 or grid[row][col-1] == 0:
-        perimeter += 1
-    
-    if col == len(grid[0]) - 1 or grid[row][col+1] == 0:
-        perimeter += 1
-    
-    if row == 0 or grid[row-1][col] == 0:
-        perimeter += 1
-        
-    if row == len(grid) - 1 or grid[row+1][col] == 0:
-        perimeter += 1
-        
+    for i, row in enumerate(grid):
+        for j, cell in enumerate(row):
+            if cell == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
     return perimeter
